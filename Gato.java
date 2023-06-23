934913223d161a0687ecec765b37ce3940cced43
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-23 15:12:43 -0300

    adicionando conteudo

---
+++ Gato.java
@@ -0,0 +1,16 @@
+package br.com.devsgeeknerd.zoologico.classes;
+
+public class Gato extends Animal {
+    public Gato(String nome) {
+        super(nome);
+    }
+
+    public Gato(String nome, int idade, String especie) {
+        super(nome, idade, especie);
+    }
+
+    public Gato(String nome, int idade) {
+        super(nome, idade, "Gato");
+    }
+
+}
